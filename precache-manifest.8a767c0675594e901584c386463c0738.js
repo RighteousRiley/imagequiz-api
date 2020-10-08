8b714017ed1e6c2faa7d91fc7d71b9e0df263d24
RighteousRiley <rcsimpso@gmail.com>
2020-10-07 22:42:08 -0700

    Updates

---
+++ precache-manifest.8a767c0675594e901584c386463c0738.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bbabbf851ec6574d77e7580a581ad15",
+    "url": "/imagequiz/index.html"
+  },
+  {
+    "revision": "6a7126a753bf4af01cfc",
+    "url": "/imagequiz/static/css/main.b72d33cd.chunk.css"
+  },
+  {
+    "revision": "07a0dabae1eb819a2e0e",
+    "url": "/imagequiz/static/js/2.e99036d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/imagequiz/static/js/2.e99036d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a7126a753bf4af01cfc",
+    "url": "/imagequiz/static/js/main.21be0f9f.chunk.js"
+  },
+  {
+    "revision": "85da51fa7abe07fb0938",
+    "url": "/imagequiz/static/js/runtime-main.2f3f5ace.js"
+  },
+  {
+    "revision": "b610ae10f73384b410deb8af2309de86",
+    "url": "/imagequiz/static/media/cherryblossom.b610ae10.png"
+  },
+  {
+    "revision": "88848efd7467dd193652b4b3b6478974",
+    "url": "/imagequiz/static/media/daffodil.88848efd.png"
+  },
+  {
+    "revision": "6f79caebdf34f3c20a49c30701002ad8",
+    "url": "/imagequiz/static/media/rose.6f79caeb.png"
+  },
+  {
+    "revision": "adc45e53918ca3fa49e002fa275e9b9b",
+    "url": "/imagequiz/static/media/sunflower.adc45e53.png"
+  },
+  {
+    "revision": "f9a3f0da6c51db0c853536791e7522c8",
+    "url": "/imagequiz/static/media/tulip.f9a3f0da.png"
+  },
+  {
+    "revision": "fca50ba10a4ca041341a13d9f650a22f",
+    "url": "/imagequiz/static/media/waterlily.fca50ba1.png"
+  }
+]);
