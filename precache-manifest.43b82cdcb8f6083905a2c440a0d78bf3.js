661fbf2c861918c0df3f2f4abb85372df0614b03
RighteousRiley <39767460+RighteousRiley@users.noreply.github.com>
2020-11-11 22:12:39 -0700

    Add build

---
+++ precache-manifest.43b82cdcb8f6083905a2c440a0d78bf3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d0b701df29f85faefc329866c3d17e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffd86f21744969e8c413",
+    "url": "/static/css/main.b03b839e.chunk.css"
+  },
+  {
+    "revision": "eefd3449119cf9d10820",
+    "url": "/static/js/2.69dc7f32.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.69dc7f32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffd86f21744969e8c413",
+    "url": "/static/js/main.85607fdf.chunk.js"
+  },
+  {
+    "revision": "ac0ec2c4f0eb2816c3eb",
+    "url": "/static/js/runtime-main.5f740883.js"
+  },
+  {
+    "revision": "746d7a769b172642a8102c675ac14d37",
+    "url": "/static/media/animals.746d7a76.jpg"
+  },
+  {
+    "revision": "c64ea3c7b8d5a66aeb18305fe84305b5",
+    "url": "/static/media/colors.c64ea3c7.jpg"
+  },
+  {
+    "revision": "511f2e8613c46b9e3086570aba100ef4",
+    "url": "/static/media/flower.511f2e86.jpg"
+  }
+]);
