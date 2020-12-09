9a3f7b480b02724af40fb727c1b0cab251d7a672
RighteousRiley <rcsimpso@gmail.com>
2020-12-09 01:27:56 -0700

    Heroku please

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 var cors = require('cors');
 let data = require('./data');
 const app = express();
-const port = 3001;
+const port = process.env.port || 3002;
 
 // middlewars
 app.use(cors());
